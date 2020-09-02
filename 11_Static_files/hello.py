344eae41597171d5d4f76d61c11677666f2eb46c
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-02 13:39:39 +0900

    Static files
    static files

---
+++ 11_Static_files/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask ,redirect, url_for, request,render_template
+from markupsafe import escape
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+   return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug = True,port=4000)
+   
+   
+   
