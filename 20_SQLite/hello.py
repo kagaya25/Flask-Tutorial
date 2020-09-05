6f846e97c5ce04a9446c56f4b4d0ccb5bad1110d
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-05 10:23:27 +0900

    Delete this later
    Delete this later need

---
+++ 20_SQLite/hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app =Flask(__name__)
+mail=Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'put you username here'
+app.config['MAIL_PASSWORD'] = 'put your password here'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'put your sender here', recipients = ['put your recipients here'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
